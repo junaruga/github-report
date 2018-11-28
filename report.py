c73bfe30128b5ec272a090bcc956ff7409b1cd8e
Jun Aruga <happy@oh4u.net>
2018-11-28 13:44:18 +0000

    Add pagination.

--- report.py
+++ report.py
@@ -1,16 +1,35 @@
 #!/usr/bin/env python3
 
+import math
 import requests
 
 USER = 'junaruga'
-SEARCH_URL_FORMAT = 'https://api.github.com/search/issues?q=user:{user}'
+SEARCH_URL_FORMAT = 'https://api.github.com/search/issues?q=user:{user}&page={page}'
+# Default items per page is 30.
+# https://developer.github.com/v3/#pagination
+PER_PAGE = 30
 
-url = SEARCH_URL_FORMAT.format(user=USER)
-
+url = SEARCH_URL_FORMAT.format(user=USER, page=1)
 response = requests.get(url)
-items = response.json()['items']
+json = response.json()
+total_count = json['total_count']
+total_page_num = math.floor(total_count / PER_PAGE) + 1
 
-for item in items:
-  title = item['title']
-  html_url = item['html_url']
-  print('- {0}\n  {1}'.format(title, html_url))
+item_count = 0
+for count in range(total_page_num):
+    page = count + 1
+
+    url = SEARCH_URL_FORMAT.format(user=USER, page=page)
+    # print('URL: {0}'.format(url))
+    response = requests.get(url)
+    json = response.json()
+
+    items = json['items']
+    for item in items:
+        title = item['title']
+        html_url = item['html_url']
+        print('- {0}\n  {1}'.format(title, html_url))
+
+        item_count += 1
+
+print('item_count: {0}'.format(item_count))
